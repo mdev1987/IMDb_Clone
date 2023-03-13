b84499cca27bd4dff86dea921d70d032a4a06c51
mdev1987 <m.developer66@gmail.com>
2023-03-14 02:02:01 +0330

    Create the Navbar component and requests.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,20 @@
+import requests from "@/utils/requests";
+import { useRouter } from "next/router";
+
 export default function Navbar() {
+    const router = useRouter();
     return (
-        <div>Navbar</div>
+        <nav className="flex justify-center 
+        bg-gray-600 text-gray-200
+        select-none text-xl lg:text-2xl">
+            {Object.entries(requests).map(([key, { title }]) => (
+                <h1 className="m-6 cursor-pointer 
+                hover:text-white active:text-red-400"
+                    onClick={() => (router.push(`?genre=${key}`))}
+                    key={key}>
+                    {title}
+                </h1>
+            ))}
+        </nav>
     )
 }
