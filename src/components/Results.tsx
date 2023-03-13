3c903392c7a76a226a975cb38ed6c99bf8fe5cf6
mdev1987 <m.developer66@gmail.com>
2023-03-14 02:20:13 +0330

    Make a request to TMDB site and fetch data

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
-export default function Results() {
+type propsType = {
+    results?: any;
+    error?: string
+}
+
+export default function Results({ results, error }: propsType) {
+    console.log(results)
+    if (error) {
+        return (
+            <h2>
+                {error}
+            </h2>
+        )
+    }
     return (
-        <div>Results</div>
+        <div>
+            {results.results.map((result: any) => (
+                <h2>{result.title}</h2>
+            ))}
+        </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import Header from '@/components/Header'
 import Navbar from '@/components/Navbar'
 import Results from '@/components/Results'
+import requests from '@/utils/requests'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
-export default function Home() {
+type propsType = {
+  results?: any;
+  error?: string
+}
+
+export default function Home(props: propsType) {
   return (
     <>
       <Head>
@@ -14,7 +21,31 @@ export default function Home() {
       </Head>
       <Header />
       <Navbar />
-      <Results />
+      <Results {...props} />
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const genre = ctx.query?.genre || 'fetchTrending';
+  const genreUrl = genre === 'fetchTopRated' ? 'fetchTopRated' : 'fetchTrending'
+  const response = await fetch(requests[genreUrl].url, {
+    method: 'GET'
+  })
+
+  if (response.ok) {
+    return {
+      props: {
+        results: await response.json(),
+        error: null
+      }
+    }
+  }
+  return {
+    props: {
+      results: null,
+      error: `Error ${response.status}: ${response.statusText}`
+    }
+  }
 }
