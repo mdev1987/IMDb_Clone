2191599e2231f4d10e5ea368a87a6cc399cc98b0
mdev1987 <m.developer66@gmail.com>
2023-03-14 01:34:41 +0330

    Style the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { HomeIcon, InformationCircleIcon, PhoneIcon, UserIcon, } from '@heroicons/react/20/solid'
-import IMDb from "../../public/assets/images/imdb.png";
+import IMDb from "../../public/assets/images/IMDb.png";
 import HeaderIcon from "./HeaderIcon";
 
 export default function Header() {
     return (
-        <header>
-            <div className="">
+        <header className="bg-gray-700 text-gray-200 
+                            flex flex-col items-center                             
+                            p-6 select-none
+                            justify-between
+                            sm:flex-row">
+            <div className="flex">
                 <HeaderIcon title="HOME" Icon={HomeIcon} />
                 <HeaderIcon title="ACCOUNT" Icon={UserIcon} />
                 <HeaderIcon title="CONTACT" Icon={PhoneIcon} />
@@ -14,7 +18,10 @@ export default function Header() {
             </div>
             <Image src={IMDb}
                 placeholder={'blur'} alt="IMDb"
-                className="w-28 h-28 object-cover" />
+                className="w-28 h-28 
+                cursor-pointer
+                active:brightness-110
+                object-contain" />
         </header>
     )
 }
--- src/components/HeaderIcon.tsx
+++ src/components/HeaderIcon.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-
 type props = {
     title: string;
     Icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>
 }
 export default function HeaderIcon({ title, Icon }: props) {
     return (
-        <div>
+        <div className="mx-4 flex flex-col 
+                      hover:text-white cursor-pointer 
+                      lg:mx-6 active:text-red-400">
             <Icon className="h-8" />
-            <p>{title}</p>
+            <p className="my-2">{title}</p>
         </div>
     )
 }
